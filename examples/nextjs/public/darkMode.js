d15ca2ab502a96c49fc7eb81974abf5080d89335
lifedup <sworden@lifedup.com>
2020-08-13 01:24:38 -0400

    updated linting

--- examples/nextjs/public/darkMode.js
+++ examples/nextjs/public/darkMode.js
@@ -1,12 +1,9 @@
-const getDefaultState = () => {
-  const lsDark = window.localStorage.getItem('dark-mode') === '1';
-  const browserIsDark = window.matchMedia('(prefers-color-scheme: dark)')
-    .matches;
-  return (browserIsDark && lsDark === null) || lsDark;
-};
+const lsDark = window.localStorage.getItem('dark-mode') === '1';
+const browserIsDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const initialState = (browserIsDark && lsDark === null) || lsDark;
 
-if (typeof BroadcastChannel !== 'undefined') {
-  const channel = new BroadcastChannel('dark-mode');
+if (typeof window.BroadcastChannel !== 'undefined') {
+  const channel = new window.BroadcastChannel('dark-mode');
   channel.onmessage = ({ data }) => {
     window.localStorage.setItem('dark-mode', data ? '1' : '0');
     document.documentElement.classList[data ? 'add' : 'remove']('__dark');
@@ -22,11 +19,9 @@ if (typeof BroadcastChannel !== 'undefined') {
   }
   window.addEventListener(visibilityChange, () => {
     if (document[hidden]) {
-      channel.postMessage(getDefaultState());
+      channel.postMessage(initialState);
     }
   });
 }
 
-document.documentElement.classList[getDefaultState() ? 'add' : 'remove'](
-  '__dark'
-);
+document.documentElement.classList[initialState ? 'add' : 'remove']('__dark');
